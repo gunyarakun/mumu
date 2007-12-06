ec4347ea5bcfb934896179a1a321f338ddcca7d7
tasuku <tasuku@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-06 04:16:54 +0000

    r260@dhcp158 (orig r3):  tasuku | 2007-09-05 19:04:49 +0900  NowNode ga ugoita !!!
    git-svn-id: http://svn.coderepos.org/share/lang/php/mumu/trunk@2532 d0d07461-0603-4401-acd4-de1884942a52

--- GunyaTemplate.php
+++ GunyaTemplate.php
@@ -71,16 +71,29 @@ class GTNode {
 
 class GTNodeList {
   private $nodes;
+  function __construct() {
+    $this->nodes = array();
+  }
   public function render($context) {
     $bits = array();
-    foreach ($nodes as $node) {
+    foreach ($this->nodes as $node) {
       array_push($bits, $node->render($context));
     }
     return implode('', $bits);
   }
+  public function push($node) {
+    array_push($this->nodes, $node);
+  }
 }
 
 class GTTextNode extends GTNode {
+  private $text;
+  function __construct($text) {
+    $this->text = $text;
+  }
+  function render($context) {
+    return $text;
+  }
 }
 
 class GTVariableNode extends GTNode {
@@ -108,12 +121,48 @@ class GTForNode extends GTNode {
 }
 
 class GTIfNode extends GTNode {
+  private $bool_exprs;
+  private $nodelist_true;
+  private $nodelist_false;
+  private $link_type;
+
+  const LINKTYPE_AND = 0;
+  const LINKTYPE_OR  = 1;
+
+  function __construct($bool_exprs, $nodelist_true, $nodelist_false, $link_type) {
+    $this->bool_exprs = $bool_exprs;
+    $this->nodelist_true = $nodelist_true;
+    $this->nodelist_false = $nodelist_false;
+    $this->link_type = $link_type;
+  }
+  function render($context) {
+    if ($this->link_type == self::LINKTYPE_OR) {
+      // TODO: 条件判断(resolve)
+      return $nodelist_false->render($context);
+    } else { // self::LINKTYPE_AND
+      // TODO: 条件判断(resolve)
+      return $nodelist_true>render($context);
+    }
+  }
 }
 
 class GTNowNode extends GTNode {
+  private $format_string;
+  function __construct($format_string) {
+    $this->format_string = $format_string;
+  }
+  function render($context) {
+    return date($this->format_string);
+  }
 }
 
-class GunyaTemplate {
+class GTUnknownNode extends GTNode {
+  function render($context) {
+    return 'unknown...';
+  }
+}
+
+class GTParser {
   private $template; // パース前のテンプレート文字列
   private $errorStr; // エラー文字列
   private $ptemplate; // パース後のテンプレート(Nodeのarray)
@@ -123,25 +172,123 @@ class GunyaTemplate {
   private $outmode;   // 'a': 追加 'i': 無視 'b': ブロックに追加
 
   # template syntax constants
-  FILTER_SEPARATOR = '|'
-  FILTER_ARGUMENT_SEPARATOR = ':'
-  VARIABLE_ATTRIBUTE_SEPARATOR = '.'
-  BLOCK_TAG_START = '{%'
-  BLOCK_TAG_END = '%}'
-  VARIABLE_TAG_START = '{{'
-  VARIABLE_TAG_END = '}}'
-  COMMENT_TAG_START = '{#'
-  COMMENT_TAG_END = '#}'
-  SINGLE_BRACE_START = '{'
-  SINGLE_BRACE_END = '}'
+  const FILTER_SEPARATOR = '|';
+  const FILTER_ARGUMENT_SEPARATOR = ':';
+  const VARIABLE_ATTRIBUTE_SEPARATOR = '.';
+  const BLOCK_TAG_START = '{%';
+  const BLOCK_TAG_END = '%}';
+  const VARIABLE_TAG_START = '{{';
+  const VARIABLE_TAG_END = '}}';
+  const COMMENT_TAG_START = '{#';
+  const COMMENT_TAG_END = '#}';
+  const SINGLE_BRACE_START = '{';
+  const SINGLE_BRACE_END = '}';
 
-  function GunyaTemplate($templatePath) {
+  // "や'でクオートされたものを除いてスペースで分割
+  // "や'内で"'を使う場合には、\"\'とする。
+  // 素直に正規表現で書けばよかったか、まあいっか。
+  private function smart_split($text) {
+    $epos = strlen($text);
+    $ret = array();
+    $mode = 'n';  // 'n': not quoted, 'd': in ", 'q': in '
+    for ($spos = 0; $spos < $epos; $spos++) {
+      $a = $text[$spos];
+      switch ($a) {
+        case '\\':
+          switch ($mode) {
+            case 'd':
+              if ($text[$spos + 1] == '"') {
+                $buf .= '"';
+                $spos += 1;
+              } else if ($text[$spos + 1] == '\\') {
+                $buf .= '\\';
+                $spos += 1;
+              } else {
+                $buf .= $a;
+              }
+              break;
+            case 'q':
+              if ($text[$spos + 1] == "'") {
+                $buf .= "'";
+                $spos += 1;
+              } else if ($text[$spos + 1] == '\\') {
+                $buf .= '\\';
+                $spos += 1;
+              } else {
+                $buf .= $a;
+              }
+              break;
+            default:
+              $buf .= '\\';
+          }
+          break;
+        case "'":
+          switch ($mode) {
+            case 'd':
+              $buf .= "'";
+              break;
+            case 'q':
+              array_push($ret, $buf);
+              $mode = 'n';
+              $buf = '';
+              break;
+            default:
+              if ($buf != '') {
+                array_push($ret, $buf);
+              }
+              $mode = 'q';
+              $buf = '';
+              break;
+          }
+          break;
+        case '"':
+          switch ($mode) {
+            case 'd':
+              array_push($ret, $buf);
+              $mode = 'n';
+              $buf = '';
+              break;
+            case 'q':
+              $buf .= '"';
+              break;
+            default:
+              if ($buf != '') {
+                array_push($ret, $buf);
+              }
+              $mode = 'd';
+              $buf = '';
+              break;
+          }
+          break;
+        case ' ':
+          switch ($mode) {
+            case 'd':
+            case 'q':
+              $buf .= ' ';
+              break;
+            default:
+              if ($buf != '') {
+                array_push($ret, $buf);
+                $buf = '';
+              }
+              break;
+          }
+          break;
+        default:
+          $buf .= $a;
+          break;
+      }
+    }
+    if ($mode == 'n' && $buf != '') {
+      array_push($ret, $buf);
+    }
+    return $ret;
   }
 
   // 終了タグを探して、その位置を返す
   private function find_closetag($t, &$spos, $closetag) {
     if (($epos = strpos($t, $closetag, $spos)) === FALSE) {
-      $this->errorStr = "タグが閉じられてないようです($closetagが見つかりません)。"
+      $this->errorStr = "タグが閉じられてないようです($closetagが見つかりません)。";
       return FALSE;
     }
     return $epos;
@@ -150,31 +297,49 @@ class GunyaTemplate {
   // {% %}の中身をパースして、GTNodeを返す。
   // extendsは頭に書かないといけない
   private function parse_block(&$spos) {
-    $spos += 2
-    if (($epos = find_closetag($this->template, $spos, self::BLOCK_TAG_END)) === FALSE) {
+    $spos += 2;
+    if (($epos = $this->find_closetag($this->template, $spos, self::BLOCK_TAG_END))
+        === FALSE) {
       return FALSE;
     }
-    $in = trim(substr($this->template, $spos, $epos));
+    $in = $this->smart_split(substr($this->template, $spos, $epos - 2));
+    echo 'inblock: '. $in[0] .'\n';
     switch ($in[0]) {
       case 'extends':
         // FIXME: ループチェック
         $node = new GTExtendsNode(nodelist, parent_name, parent_name_expr);
+        break;
       case 'include':
         // FIXME: ループチェック
         $node = new GTIncludeNode(template_name);
+        break;
       case 'block': // endblock
-        $node = new GTBlockNode(name, nodelist, parent=None);
+        $node = new GTBlockNode(name, nodelist, parent);
+        break;
       case 'for': // endfor
         $node = new GTForNode(loopvar, sequence, reversed, nodelist_loop);
+        break;
       case 'cycle':
         $node = new GTCycleNode(cyclevars);
         // TODO: implement
+        break;
       case 'if': // else, endif
         $node = new GTIfNode(bool_exprs, nodelist_true, nodelist_false, link_type);
+        break;
       case 'debug':
         $node = new GTDebugNode();
+        break;
       case 'now':
-        $node = new GTNowNode(format_string);
+        var_dump($in);
+        if (count($in) != 2) {
+          $this->errorStr = 'nowは書式文字列が必要です';
+          return FALSE;
+        }
+        $node = new GTNowNode($in[1]);
+        break;
+      default:
+        $node = new GTUnknownNode();
+        break;
     }
     $spos = $epos + 2;
     return $node;
@@ -187,7 +352,7 @@ class GunyaTemplate {
 
   // {{ }}の中身をパース
   private function parse_variable(&$spos) {
-    $spos += 2
+    $spos += 2;
     if (($epos = find_closetag($this->template, $spos, self::VARIABLE_TAG_END)) === FALSE) {
       return FALSE;
     }
@@ -199,48 +364,69 @@ class GunyaTemplate {
 
   // {# #}の中身をパース
   private function parse_comment(&$spos) {
-    $spos += 2
+    $spos += 2;
     if (($epos = find_closetag($this->template, $spos, self::COMMENT_TAG_END)) === FALSE) {
       return FALSE;
     }
     $spos = $epos + 2; // #}のあと
   }
 
-  public function parse($templatePath) {
-    if (($t = $file_get_contents($templatePath)) === FALSE) {
+  public function parse_from_file($templatePath) {
+    if (($t = file_get_contents($templatePath)) === FALSE) {
       $this->errorStr = 'ファイルが開けません。';
       return FALSE;
     }
-    $pos = 0;
-
     $this->template = $t;
+    return $this->_parse(0, strlen($t));
+  }
 
-    // タグの開始部分を見つける
-    while (($pos = strpos($t, self::SINGLE_BRACE_START, $pos)) !== FALSE) {
-      switch (substr($t, pos, 2) {
-        case BLOCK_TAG_START:
-          if (parse_block($pos) === FALSE) {
+  public function parse($templateStr) {
+    $this->template = $templateStr;
+    return $this->_parse(0, strlen($templateStr));
+  }
+
+  private function _parse($spos, $epos) {
+    $nl = new GTNodeList();
+    while ($spos < $epos) {
+      // タグの開始部分を見つける
+      $nspos = strpos($this->template, self::SINGLE_BRACE_START, $spos);
+      if ($nspos === FALSE) {
+        $nspos = $epos;
+      }
+      if ($spos < $nspos) {
+        // タグ以外の部分をGTTextNodeとして保存
+        $nl->push(new GTTextNode(substr($this->template, $spos, $nspos - $spos)));
+      }
+      switch (substr($this->template, $nspos, 2)) {
+        case self::BLOCK_TAG_START:
+          if (($node = $this->parse_block($nspos)) === FALSE) {
             unset($this->template);
             return FALSE;
           }
+          $nl->push($node);
           break;
-        case VARIABLE_TAG_START:
-          if (parse_variable($pos) === FALSE) {
+        case self::VARIABLE_TAG_START:
+          if (($node = $this->parse_variable($nspos)) === FALSE) {
             unset($this->template);
             return FALSE;
           }
+          $nl->push($node);
           break;
-        case COMMENT_TAG_START:
-          if (parse_comment($pos) === FALSE) {
+        case self::COMMENT_TAG_START:
+          if (($node = $this->parse_comment($nspos)) === FALSE) {
             unset($this->template);
             return FALSE;
           }
+          $nl->push($node);
           break;
         default:
           // 単なる{は読みとばす
-          $pos += 1;
+          $nspos += 1;
       }
+      $spos = $nspos;
     }
+    return $nl;
   }
+
 }
 ?>
